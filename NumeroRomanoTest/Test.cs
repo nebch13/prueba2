c45c859a71e19a302175072d0ec64dbd665429d3
nebch13 <noet_09@hotmail.com>
2014-10-11 16:55:08 -0500

    primer commit

---
+++ NumeroRomanoTest/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NumerosRomanos;
+
+namespace NumeroRomanoTest
+{
+    [TestFixture]
+   public class Test
+    {
+
+        [Test]
+        public void TestNumero6()
+        { 
+            //arranque
+            var roman = new CambioRomano();
+
+            var num = roman.GetRomanStringNumber(6);
+            Assert.AreEqual("VI", num);
+        }
+    }
+}
---
+++ NumerosRomanos/CambioRomano.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosRomanos
+{
+   public class CambioRomano
+    {
+
+       public String GetRomanStringNumber(int num)
+       {
+           var solution ="";
+           while (num > 0)
+           {
+               if (num>=10)
+               {
+                   solution += "X";
+                   num -= 10;
+               }
+
+           
+              else if  (num >= 5)
+               {
+                   solution += "V";
+                   num -= 5;
+               }
+                   else
+	                {
+                    if (num ==4)
+	                    {
+                            solution += "IV";
+                            num -= 4;
+	                    }
+                    else
+                    {
+                        solution += "I";
+                        num--;
+                    }
+	                }
+               
+           }
+           return solution;
+       }
+
+      
+       
+      
+    }
+}
